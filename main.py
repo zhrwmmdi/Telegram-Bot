b7435dc182923365f7daea1ad1187be487300bed
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-09-06 17:41:07 +0330

    logging setting added

---
+++ main.py
@@ -0,0 +1,16 @@
+import logging
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from local_utils import TOKEN
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+
