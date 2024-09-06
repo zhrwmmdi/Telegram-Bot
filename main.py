ed93f82fb62048c3ec9afe65ac6cb5ea2f840e1b
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-09-06 18:57:21 +0330

    some other handlers added

--- main.py
+++ main.py
@@ -56,12 +56,30 @@ async def help_command_handler(update: Update, context: ContextTypes.DEFAULT_TYP
         reply_to_message_id=update.effective_message.id
     )
 
+
+async def upper_case_message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=update.message.text.upper(),
+        reply_to_message_id=update.effective_message.id
+    )
+
+
+async def echo_sticker_message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_sticker(
+        chat_id=update.effective_chat.id,
+        sticker=update.effective_message.sticker,
+        reply_to_message_id=update.effective_message.id
+    )
+
 if __name__=="__main__":
     # Create the Application and pass it your bot's token
     application=Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler('start', start_handler))
     # application.add_handler(MessageHandler(filters.TEXT, echo_handler))
+    application.add_handler(MessageHandler(filters.TEXT, upper_case_message_handler))
+    application.add_handler(MessageHandler(filters.Sticker.ALL, echo_sticker_message_handler))
     application.add_handler(CommandHandler('repeat', repeat_command_handler))
     application.add_handler(CommandHandler('help', help_command_handler))
     application.add_handler(CommandHandler('time', time_command_handler))
