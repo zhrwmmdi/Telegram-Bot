c70fcececf450563f5fcd7e29261bdb7b58973ab
zhrwmmdi <zahra.mmdi2003@gmail.com>
2024-09-06 17:41:54 +0330

    /start command added for saying hello to user

--- main.py
+++ main.py
@@ -13,4 +13,19 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 
+async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'Hello dear {update.effective_user.first_name}'
+    )
+
+
+
+if __name__=="__main__":
+    # Create the Application and pass it your bot's token
+    application=Application.builder().token(TOKEN).build()
+
+    application.add_handler(CommandHandler('start', start_handler))
+    # Run the Bot
+    application.run_polling()
 
